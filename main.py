144b2a96834a7dd46b2f491cfddbe962fc283ee2
Scott Heartwood <Scott.Heartwood@outlook.com>
2025-12-08 21:52:28 +1100

    Add usage metadata.

--- main.py
+++ main.py
@@ -14,12 +14,28 @@ def main():
     # Create a new instance of Gemini Client
     client = genai.Client(api_key=api_key)
 
+    # Define a prompt to be passed to the AI Agent
+    prompt = 'Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+
     # Get a response from client
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.5-flash', contents=prompt
     )
-    print(response.text)
 
+    # Verify that the usage metadata property is not None.
+    if not(response.usage_metadata):
+        raise RuntimeError("Gemini API call error")
+
+    # Extract the prompt token count and the candidate token count
+    prompt_token_count = response.usage_metadata.prompt_token_count
+    candidate_token_count = response.usage_metadata.candidates_token_count
+
+    # Print out response:
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {prompt_token_count}")
+    print(f"Response tokens: {candidate_token_count}")
+    print(f"Response:")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
